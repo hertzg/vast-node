d7ef307638df02a0b58be6659d3a54ba6059abcb
George Hertz <hertzg@users.noreply.github.com>
2025-06-10 15:09:43 +0200

    chore: remove dist from repo

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @file index.ts
- * @description Entry point for the Vast.ai Node.js SDK
- */
-export { VastClient } from './vast-client';
-export { MachineOffer, Instance, DockerImage, UserInfo, SearchOffersParams, CreateInstanceParams, ListInstancesParams, ApiError } from './types';
-export { DynamicApi, ApiConfig, EndpointConfig } from './dynamic-api';
--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * @file types.ts
- * @description TypeScript interface definitions for Vast.ai API
- */
-Object.defineProperty(exports, "__esModule", { value: true });
